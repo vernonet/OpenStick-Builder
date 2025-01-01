f6d118d28519195806e09b0781f44153616902cb
vernonet <49722354+vernonet@users.noreply.github.com>
2025-01-01 16:54:20 +0200

    Update debootstrap.sh

--- scripts/debootstrap.sh
+++ scripts/debootstrap.sh
@@ -69,7 +69,7 @@ mkdir -p ${CHROOT}/lib/firmware/msm-firmware-loader
 
 # testing copy leds config files
 cp leds_config/leds_config.sh ${CHROOT}/home/user
-cp leds_config/leds_config.service ${CHROOT}/etc/systemmd/system
+cp leds_config/leds_config.service ${CHROOT}/etc/systemd/system
 
 # update fstab
 echo "PARTUUID=80780b1d-0fe1-27d3-23e4-9244e62f8c46\t/boot\text2\tdefaults\t0 2" > ${CHROOT}/etc/fstab
