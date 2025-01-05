479928fa832949ca33652a7562f0d454655ec284
vernonet <49722354+vernonet@users.noreply.github.com>
2025-01-05 17:35:35 +0200

    Update leds_config.sh
    replace phy0radio > rfkill-any

--- leds_config/leds_config.sh
+++ leds_config/leds_config.sh
@@ -2,7 +2,7 @@
 
 echo usb-gadget >  /sys/class/leds/green:sms/trigger
 echo phy0tx >      /sys/class/leds/green:wlan/trigger
-echo phy0radio >   /sys/class/leds/blue:wan_blue/trigger
+echo rfkill-any >  /sys/class/leds/blue:wan_blue/trigger
 
 
  
