2068f538a0d759e9d647f18d87805a44ef4c1b20
vernonet <49722354+vernonet@users.noreply.github.com>
2026-03-16 10:30:46 +0200

    Update LED triggers in leds_config_new.sh

--- leds_config/leds_config_new.sh
+++ leds_config/leds_config_new.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
 
-echo usb-gadget >  /sys/class/leds/green:sms/trigger
-echo phy0tx >      /sys/class/leds/green:wlan/trigger
-echo rfkill-any >  /sys/class/leds/blue:wan_blue/trigger
+echo usb-gadget >  /sys/class/leds/green:mail/trigger
+echo phy0tx >  /sys/class/leds/green:wlan/trigger
+echo rfkill-any >  /sys/class/leds/blue:status/trigger
